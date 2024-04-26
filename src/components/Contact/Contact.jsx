7c2cc3299b98a00cabad4c967710bd52d515e953
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-04-25 23:15:09 -0300

    adding links and tooltips to the contact buttons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,59 @@
 import { icons } from "../../assets/icons";
 import "./Contact.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Tooltip } from "react-tooltip";
 
 const Contact = () => {
   return (
     <div className="contact_main_container">
       <div className="buttons_container">
-        <button>{icons.email}</button>
-        <a href="">{icons.linkedin}</a>
-        <a href="">{icons.twitter}</a>
-        <a href="">{icons.github}</a>
+        <CopyToClipboard
+          text="francisco.brito.developer@gmail.com"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Copiar correo"
+          data-tooltip-place="top"
+        >
+          <button className="contact_btns">{icons.email}</button>
+        </CopyToClipboard>
+        <a
+          className="contact_btns"
+          href="https://www.linkedin.com/in/francisco-javier-brito-b4657420a/"
+          target="blank"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Linkedin"
+          data-tooltip-place="top"
+        >
+          {icons.linkedin}
+        </a>
+        <a
+          className="contact_btns"
+          href="https://twitter.com/Francis86056304"
+          target="blank"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Twitter"
+          data-tooltip-place="top"
+        >
+          {icons.twitter}
+        </a>
+        <a
+          className="contact_btns"
+          href="https://github.com/FranciscoJBrito"
+          target="blank"
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content="Github"
+          data-tooltip-place="top"
+        >
+          {icons.github}
+        </a>
+        <Tooltip
+          id="my-tooltip"
+          style={{
+            borderRadius: "18",
+            fontFamily: "Pixel Font7",
+            background: "#38BC36",
+            transition: "all 0.5s ease",
+          }}
+        />
       </div>
     </div>
   );
