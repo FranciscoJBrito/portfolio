5710eb451fbd539be9b1a1df1d9f9e5076c2c2b8
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-04-26 23:28:56 -0300

    adding blinking cursor

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,13 @@
 import "./Hero.css";
 
 const Hero = () => {
+
   return (
     <div className="main_hero_container">
       <h1 className="hero_title">
-        HOLA YO
+        HOLA, YO
         <br />
-        SOY FRAN
+        SOY FRAN<span className="blinking_cursor">|</span>
       </h1>
     </div>
   );
