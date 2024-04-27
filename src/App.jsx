cd08cf4817c58e8dda30b7ef90b502287b3330fe
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-04-26 23:24:20 -0300

    adding h2 to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import './App.css'
-import Contact from './components/Contact/Contact'
-import Nabvar from './components/NavBar/NavBar'
-import Hero from './components/Hero/Hero'
-import Presentation from './components/Presentation/Presentation'
-import Mouse from './components/Mouse/Mouse'
+import "./App.css";
+import Contact from "./components/Contact/Contact";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import Presentation from "./components/Presentation/Presentation";
 
 function App() {
-
   return (
     <>
-      <Nabvar />
+      <NavBar />
       <Hero />
       <Presentation />
+      <h2 className="query_project_title">SELECT <span>*</span> FROM <span style={{fontFamily: "Pixel Font7"}}>projects</span></h2>
       <Contact />
-      <Mouse />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
