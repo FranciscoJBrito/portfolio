1c52b6c04b55f04b61cb28176b14de504ca4b2e9
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-04-28 23:11:51 -0300

    adding max width to the body and adding Pixel font

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import Contact from "./components/Contact/Contact";
-import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Presentation from "./components/Presentation/Presentation";
 
 function App() {
   return (
     <>
-      <NavBar />
       <Hero />
       <Presentation />
       <h2 className="query_project_title">SELECT <span>*</span> FROM <span style={{fontFamily: "Pixel Font7"}}>projects</span></h2>
