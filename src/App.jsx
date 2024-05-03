3acdb219b32d355945b86074d593d603b20bae40
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-05-02 21:46:11 -0300

    adding popup for project overview

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 import Contact from "./components/Contact/Contact";
 import Hero from "./components/Hero/Hero";
 import Presentation from "./components/Presentation/Presentation";
+import Popup from "./layouts/ProjectView/components/Popup";
 import Projects from "./layouts/Projects/Projects";
 
 function App() {
+  const [popup, setPopup] = useState(false);
+  const handlePopup = () => {
+    setPopup(!popup);
+  };
+
   return (
     <>
       <Hero />
       <Presentation />
-      <h2 className="query_project_title">SELECT <span>*</span> FROM <span style={{fontFamily: "Pixel Font7"}}>projects</span></h2>
-      <Projects />
-      <br />
-      <br />
+      <h2 className="query_project_title">
+        SELECT <span>*</span> FROM{" "}
+        <span style={{ fontFamily: "Pixel Font7" }}>projects</span>
+      </h2>
+      <Projects handle={handlePopup} />
+      <Popup
+        title={"Airbnb Clone"}
+        image={"./mockup-airbnb-light.png"}
+        tech={"docker, rails, postgresql"}
+        state={popup}
+        handle={handlePopup}
+      />
       <Contact />
     </>
   );
